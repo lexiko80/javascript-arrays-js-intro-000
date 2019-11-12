ce5d1b7f093c869942d51f0764b931ce06ece7e3
Alexis Kotzambasis <lexiko80@gmail.com>
2019-11-12 00:20:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,8 @@ var chocolateBars = ['snickers',
 'hundred grand',
 'kitkat',
 'skittles']
+
+addElementToBeginningOfArray => (theArray, theElement){
+  newArray = [...theArray, theElement];
+  return newArray 
+}
